be2692c215952b4231bd8abc2cae393abcf9daa4
SunCubeIsMyID <38004925+SunCubeIsMyID@users.noreply.github.com>
2019-03-23 23:34:41 +0800

    Add files via upload

---
+++ HW3/PrimePathTest.java
@@ -0,0 +1,15 @@
+package HW3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Junit.money;
+
+public class PrimePathTest {
+
+	@Test
+    public void primepathtest() {
+		assertEquals(11,new PrintPrimes().printPrimes(5));
+	}
+}
